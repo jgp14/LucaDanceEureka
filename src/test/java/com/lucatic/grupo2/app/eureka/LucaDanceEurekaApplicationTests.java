25d0041371a161aea36585b21668e81a4edd8761
jgp14 <jaimegarciaparada@gmail.com>
2024-03-14 09:47:42 +0100

    merge remote to local

---
+++ src/test/java/com/lucatic/grupo2/app/eureka/LucaDanceEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucatic.grupo2.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucaDanceEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
