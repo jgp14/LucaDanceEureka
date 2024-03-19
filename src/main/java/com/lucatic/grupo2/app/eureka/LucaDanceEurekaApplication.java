85ef089ff5449a7a1ed5173f33fe4ce3caf30a6e
jgp14 <jaimegarciaparada@gmail.com>
2024-03-19 16:33:39 +0100

    Eureka server anotations

--- src/main/java/com/lucatic/grupo2/app/eureka/LucaDanceEurekaApplication.java
+++ src/main/java/com/lucatic/grupo2/app/eureka/LucaDanceEurekaApplication.java
@@ -4,10 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Clase principal del Eureka
+ *
+ * @author BlueDevTeam
+ * @version 1.0.0
+ * @since 15-03-2024
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class LucaDanceEurekaApplication {
 
+	/**
+	 * Punto de entrada al programa principal del EurekaServer
+	 * 
+	 * @param args Argumentos por línea de comandos
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(LucaDanceEurekaApplication.class, args);
 	}
